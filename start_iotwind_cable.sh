9537f0995659f48d1aad8624e8a18d12e95e611c
IoT Wind <iotwind.user.mweis@spamgourmet.com>
2019-01-30 15:19:22 +0100

    launcher added

---
+++ start_iotwind_cable.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+gnome-terminal -e '~/anemometer3d/read_wind.py -l DEBUG -c ~/anemometer3d/richtungen.json'
+
---
+++ start_iotwind_wifi.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+gnome-terminal -e '~/anemometer3d/read_wind.py -s 10.0.0.1 -l DEBUG -c ~/anemometer3d/richtungen.json'
+
